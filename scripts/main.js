d982429ba568f4e104adf7e97bbfc7e13619144e
Tracy Ly <tly35@my.bcit.ca>
2022-03-01 19:32:27 -0800

    demo09 bonus: add hike cards

--- scripts/main.js
+++ scripts/main.js
@@ -38,3 +38,69 @@ function insertName() {
   });
 }
 insertName();
+
+
+function writeHikes() {
+  //define a variable for the collection you want to create in Firestore to populate data
+  var hikesRef = db.collection("hikes");
+
+  hikesRef.add({
+      code:"BBY01",
+      name: "Fancy Lake Trail",
+      city: "Burnaby",
+      province: "BC",
+      level: "easy",
+      length: "10 km",
+      details: "Tracy does not go here regularly"
+  });
+  hikesRef.add({
+      code:"AM01",
+      name: "Pretty Trail",
+      city: "Anmore",
+      province: "BC",
+      level: "moderate",
+      length: "10.5 km",
+      details: "Tracy goes here regularly"
+  });
+  hikesRef.add({
+      code:"NV01",
+      name: "Decent Trail",
+      city: "North Vancouver",
+      province: "BC",
+      level: "hard",
+      length: "8.2 km",
+      details: "Tracy does not go here regularly"
+  });
+}
+//writeHikes();       //if you used method 1
+
+//Display cards
+function displayCards(collection) {
+  let cardTemplate = document.getElementById("hikeCardTemplate");
+  
+  db.collection(collection).get()
+      .then(snap => {
+          var i = 1;
+          snap.forEach(doc => { //iterate thru each doc
+              var title = doc.data().name;   // get value of the "name" key
+              var details = doc.data().details;   // get value of the "details" key
+              let newcard = cardTemplate.content.cloneNode(true);
+
+              //update title and text and image
+              newcard.querySelector('.card-title').innerHTML = title;
+              newcard.querySelector('.card-text').innerHTML = details;
+              newcard.querySelector('.card-image').src = "./images/" + collection + "_" + doc.data().code + ".jpg"; //hikes.jpg
+
+              //give unique ids to all elements for future use
+              // newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+              // newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+              // newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+              //attach to gallery
+              document.getElementById(collection + "-go-here").appendChild(newcard);
+              i++;
+          })
+      })
+}
+
+displayCards("hikes");
